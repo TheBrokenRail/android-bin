b72c9a097c6d4e30700b536b20ec1bb36d38538b
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-03 14:11:42 -0400

    Create get-ndk.sh

---
+++ get-ndk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo 'Downloading NDK...'
+NDK_VER='android-ndk-r18b'
+curl --retry 5 -L -o ndk.zip "https://dl.google.com/android/repository/${NDK_VER}-linux-x86_64.zip"
+unzip ndk.zip > /dev/null
+export NDK_HOME=$(pwd)/${NDK_VER}
+
+echo 'Building Toolchain...'
+${NDK_HOME}/build/tools/make_standalone_toolchain.py \
+  --arch=${ARCH} \
+  --api=21 \
+  --install-dir=${NDK_HOME}/generated-toolchains/${ARCH}
+export PATH=${NDK_HOME}/generated-toolchains/${ARCH}/bin:${PATH}
+export TARGET=$(cd ${NDK_HOME}/build/tools; python -c 'import make_standalone_toolchain; print make_standalone_toolchain.get_triple("arm")')
+
+echo 'Creationg CMake Toolchain File...'
+echo -e 'set(CMAKE_SYSTEM_NAME Android)\nset(CMAKE_ANDROID_STANDALONE_TOOLCHAIN '"${NDK_HOME}/generated-toolchains/${ARCH}"')' > toolchain.cmake
+
+echo "NDK_HOME: ${NDK_HOME}"
+echo "TARGET: ${TARGET}"
