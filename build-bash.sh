df982a52a0622c880c29e975eadce929c9312a87
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-03 18:55:18 -0400

    Create build-bash.sh

---
+++ build-bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+OUT_DIR=${DEPLOY_DIR}/bash
+mkdir ${OUT_DIR}
+BASH_VER=4.4
+curl --retry 5 -L -o bash.tar.gz hhttp://ftp.gnu.org/gnu/bash/bash-${BASH_VER}.tar.gz
+tar zxf bash.tar.gz
+cd bash-${BASH_VER}
+
+./configure --host=${TARGET} --prefix=${OUT_DIR}
+make
+make install
