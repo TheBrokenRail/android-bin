ccb63f575250034598c1117918edb457ad53bfa6
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-03 16:27:01 -0400

    Create build-lua-jit.sh

---
+++ build-lua-jit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+OUT_DIR=${DEPLOY_DIR}/lua-jit
+mkdir ${OUT_DIR}
+LUA_VER=2.0.5
+curl --retry 5 -L -o lua-jit.tar.gz http://luajit.org/download/LuaJIT-${LUA_VER}.tar.gz
+tar zxf lua-jit.tar.gz
+cd LuaJIT-${LUA_VER}
+
+make CROSS=${TARGET}- PREFIX=${OUT_DIR}
