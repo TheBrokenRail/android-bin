674f38e7eeef1aa2c0f0514a570639eba1362fa9
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-03 19:09:47 -0400

    Update build-bash.sh

--- build-bash.sh
+++ build-bash.sh
@@ -5,7 +5,7 @@ set -e
 OUT_DIR=${DEPLOY_DIR}/bash
 mkdir ${OUT_DIR}
 BASH_VER=4.4
-curl --retry 5 -L -o bash.tar.gz hhttp://ftp.gnu.org/gnu/bash/bash-${BASH_VER}.tar.gz
+curl --retry 5 -L -o bash.tar.gz http://ftp.gnu.org/gnu/bash/bash-${BASH_VER}.tar.gz
 tar zxf bash.tar.gz
 cd bash-${BASH_VER}
 
