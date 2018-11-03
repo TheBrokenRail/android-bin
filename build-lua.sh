02de641e23bd080c415be13b6777cc475db7cec0
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-03 14:49:18 -0400

    Create build-lua.sh

---
+++ build-lua.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+OUT_DIR=${DEPLOY_DIR}/lua
+mkdir ${OUT_DIR}
+LUA_VER=5.3.5
+curl --retry 5 -L -o lua.tar.gz http://www.lua.org/ftp/lua-${LUA_VER}.tar.gz
+tar zxf lua.tar.gz
+cd lua-${LUA_VER}
+
+make linux local CC=${TARGET}-gcc
+cp -r ./install/* ${OUT_DIR}
