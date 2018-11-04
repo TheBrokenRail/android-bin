5b6f0fe6e3e1b3e172a1d922250f32a267eb869c
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-04 17:21:01 -0500

    Update build-jerryscript.sh

--- build-jerryscript.sh
+++ build-jerryscript.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 set -e
-set -x
 
 OUT_DIR=${DEPLOY_DIR}/jerryscript
 mkdir ${OUT_DIR}
 git clone --depth=1 https://github.com/jerryscript-project/jerryscript.git
 cd jerryscript
 
-echo ${TOOLCHAIN_FILE}
-cat ${TOOLCHAIN_FILE}
 python tools/build.py --toolchain=${TOOLCHAIN_FILE} --install=${OUT_DIR}
