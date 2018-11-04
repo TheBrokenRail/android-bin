3ac4fc53c114842c816369524574b47fcdf9fa77
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-04 17:40:31 -0500

    Update get-ndk.sh

--- get-ndk.sh
+++ get-ndk.sh
@@ -9,15 +9,17 @@ unzip ndk.zip > /dev/null
 export NDK_HOME=$(pwd)/${NDK_VER}
 
 echo 'Building Toolchain...'
+export TOOLCHAIN_ROOT=${NDK_HOME}/generated-toolchains/${ARCH}
 ${NDK_HOME}/build/tools/make_standalone_toolchain.py \
   --arch=${ARCH} \
   --api=21 \
-  --install-dir=${NDK_HOME}/generated-toolchains/${ARCH}
+  --install-dir=${TOOLCHAIN_ROOT}
 export PATH=${NDK_HOME}/generated-toolchains/${ARCH}/bin:${PATH}
 export TARGET=$(cd ${NDK_HOME}/build/tools; python -c 'import make_standalone_toolchain; print make_standalone_toolchain.get_triple("'"${ARCH}"'")')
 
 echo 'Creationg CMake Toolchain File...'
-echo -e 'set(CMAKE_SYSTEM_NAME Android)\nset(CMAKE_ANDROID_STANDALONE_TOOLCHAIN '"${NDK_HOME}/generated-toolchains/${ARCH}"')' > toolchain.cmake
+echo -e 'set(CMAKE_SYSTEM_NAME Android)\nset(CMAKE_ANDROID_STANDALONE_TOOLCHAIN '"${TOOLCHAIN_ROOT}"')' > toolchain.cmake
+ls ${TOOLCHAIN_ROOT}/sysroot/android
 export TOOLCHAIN_FILE=$(pwd)/toolchain.cmake
 
 echo "NDK_HOME: ${NDK_HOME}"
