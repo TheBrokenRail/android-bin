f375d7368ee453bdd593a51a324569a6d9952c38
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-04 18:15:40 -0500

    Update get-ndk.sh

--- get-ndk.sh
+++ get-ndk.sh
@@ -19,7 +19,7 @@ export TARGET=$(cd ${NDK_HOME}/build/tools; python -c 'import make_standalone_to
 
 echo 'Creationg CMake Toolchain File...'
 echo -e 'set(CMAKE_SYSTEM_NAME Android)\nset(CMAKE_ANDROID_STANDALONE_TOOLCHAIN '"${TOOLCHAIN_ROOT}"')' > toolchain.cmake
-ls ${TOOLCHAIN_ROOT}/sysroot/android || sleep 20
+ls ${TOOLCHAIN_ROOT} || sleep 60
 export TOOLCHAIN_FILE=$(pwd)/toolchain.cmake
 
 echo "NDK_HOME: ${NDK_HOME}"
