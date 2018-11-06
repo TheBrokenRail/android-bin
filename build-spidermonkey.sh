68a5563e71a02c1aa7c03f6be9449b7dda55bea5
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-06 10:48:18 -0500

    Create build-spidermonkey.sh

---
+++ build-spidermonkey.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+OUT_DIR=${DEPLOY_DIR}/spidermonkey
+mkdir ${OUT_DIR}
+git clone --depth=1 https://github.com/mozilla/gecko-dev.git
+cd gecko-dev/js/src
+
+autoconf-2.13
+
+mkdir build
+cd build
+../configure \
+  --target=${TARGET} \
+  --with-android-ndk=${NDK_HOME}
+  --prefix=${OUT_DIR}
+make
+make install
