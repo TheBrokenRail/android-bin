ff5945b4ec1ffa237baabc2756cd6bd387c1e18d
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-06 09:54:58 -0500

    Update build-jerryscript.sh

--- build-jerryscript.sh
+++ build-jerryscript.sh
@@ -7,4 +7,4 @@ mkdir ${OUT_DIR}
 git clone --depth=1 https://github.com/jerryscript-project/jerryscript.git
 cd jerryscript
 
-python tools/build.py --toolchain=${TOOLCHAIN_FILE} --install=${OUT_DIR} --debug
+python tools/build.py --toolchain=${TOOLCHAIN_FILE} --install=${OUT_DIR} --cmake-param=-DENABLE_LTO=OFF
