025f1d88e304d1889e5dd879a4402b61ff14bd46
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-21 13:25:40 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,20 +2,21 @@
 
 set +e
 
-EXIT=0
-
 run() {
   local RESET='\033[0m'
-  local COLOR='\e[32m\e[1m'
-  echo -e "${COLOR}Running $1${RESET}"
+  local START='\e[33m\e[1m'
+  local FAIL='\e[31m\e[1m'
+  local SUCCESS='\e[32m\e[1m'
+  echo -e "${START}Running $1${RESET}"
   $1
   if [[ $? != 0 ]]; then
-    EXIT=1
+    echo -e "${FAIL}$1 Failed${RESET}"
+  fi
+  if [[ $? == 0 ]]; then
+    echo -e "${SUCCESS}$1 Succeeded${RESET}"
   fi
 }
 
 run ./build-jerryscript.sh
 run ./build-lua.sh
 run ./build-python.sh
-
-exit ${EXIT}
