225d2fef6d95fd58688a19662acbeaa901f5d4c6
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-21 12:47:46 -0500

    Create build-python.sh

---
+++ build-python.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+OUT_DIR=${DEPLOY_DIR}/python
+mkdir ${OUT_DIR}
+
+pip3 install git+https://github.com/kivy/python-for-android.git
+mkdir -p py-build/build/python-installs/build
+
+p4a create --sdk-dir ${ANDROID_HOME} --ndk-dir ${NDK_HOME} --ndk-version ${NDK_VER} --android-api 26 --arch ${ABI} --requirements python3 --storage-dir $(pwd)/py-build --dist-name build
+cp -r py-build/* ${OUT_DIR}
