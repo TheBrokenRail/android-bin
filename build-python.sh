82e97fda457aa63a770dbdca5141cfb33c31d2af
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-29 12:50:54 -0800

    Update build-python.sh

--- build-python.sh
+++ build-python.sh
@@ -11,12 +11,12 @@ mkdir -p py-build/build/python-installs/build
 p4a create --sdk-dir ${ANDROID_HOME} --ndk-dir ${NDK_HOME} --ndk-version ${NDK_VER} --android-api 26 --ndk-api 21 --arch ${ABI} --requirements python3 --storage-dir $(pwd)/py-build --dist-name build
 
 PY_INCLUDE=py-build/build/other_builds/python3/${ABI}__ndk_target_${API_LEVEL}/python3/Include
-PY_LIBS=py-build/dists/build/libs/${ABI}
+PY_LIBS=py-build/build/other_builds/python3/${ABI}__ndk_target_${API_LEVEL}/python3/android-build
 PY_BUNDLE=py-build/dists/build/_python_bundle/_python_bundle
 
 mkdir ${OUT_DIR}/include
 cp -r ${PY_INCLUDE}/* ${OUT_DIR}/include
 mkdir ${OUT_DIR}/libs
-cp ${PY_LIBS}/* ${OUT_DIR}/libs
+cp ${PY_LIBS}/*.a ${OUT_DIR}/libs
 mkdir ${OUT_DIR}/bundle
 cp -r ${PY_BUNDLE}/* ${OUT_DIR}/bundle
