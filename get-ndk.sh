1fc2fd1015bb25fc8bb94dc8e8bbe9b5b310ebfd
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-12-01 22:30:57 -0500

    Update get-ndk.sh

--- get-ndk.sh
+++ get-ndk.sh
@@ -12,7 +12,7 @@ export NDK_HOME=$(pwd)/crystax-ndk-${NDK_VER}
 
 echo 'Building Toolchain...'
 export TOOLCHAIN_ROOT="${NDK_HOME}/generated-toolchains/${ARCH}"
-${NDK_HOME}/build/tools/make_standalone_toolchain.py \
+${NDK_HOME}/build/tools/make-standalone-toolchain.sh \
   --arch=${ARCH} \
   --api=${API_LEVEL} \
   --install-dir=${TOOLCHAIN_ROOT}
@@ -21,7 +21,7 @@ export TARGET=$(cd ${NDK_HOME}/build/tools; python -c 'import make_standalone_to
 export ABI=$(cd ${NDK_HOME}/build/tools; python -c 'import make_standalone_toolchain; print make_standalone_toolchain.get_abis("'"${ARCH}"'")[0]')
 
 echo 'Creationg CMake Toolchain File...'
-echo -e 'set(CMAKE_C_COMPILER '"${TARGET}"'-gcc)\nset(CMAKE_C_FLAGS -O2)\nset(CMAKE_SYSTEM_NAME Linux)\nset(CMAKE_SYSROOT '"${TOOLCHAIN_ROOT}"'/platforms/android-'"${API_LEVEL}"'/arch-'"${ARCH}"')' > toolchain.cmake
+echo -e 'set(CMAKE_C_COMPILER '"${TARGET}"'-gcc)\nset(CMAKE_C_FLAGS -O2)\nset(CMAKE_SYSTEM_NAME Linux)\nset(CMAKE_SYSROOT '"${TOOLCHAIN_ROOT}"'/sysroot)' > toolchain.cmake
 export TOOLCHAIN_FILE=$(pwd)/toolchain.cmake
 
 echo "NDK_HOME: ${NDK_HOME}"
