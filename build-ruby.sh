efe92ae410c06aa485f52bb6274cbd580bafd2bb
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-12-17 21:30:03 -0500

    Update build-ruby.sh

--- build-ruby.sh
+++ build-ruby.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+RUBY_VER=2.5
+RUBY_VER_MINOR=3
+
 git clone https://github.com/rbenv/ruby-build.git
 mkdir ruby-build/prefix
 PREFIX=$(pwd)/ruby-build/prefix ruby-build/install.sh
-ruby-build 2.5.3 $(pwd)/ruby
+ruby-build ${RUBY_VER}.${RUBY_VER_MINOR} $(pwd)/ruby
 
 OUT_DIR=${DEPLOY_DIR}/ruby
 mkdir ${OUT_DIR}
-RUBY_VER=2.5
-RUBY_VER_MINOR=3
 curl --retry 5 -L -o ruby.tar.gz https://cache.ruby-lang.org/pub/ruby/${RUBY_VER}/ruby-${RUBY_VER}.${RUBY_VER_MINOR}.tar.gz
 tar zxf ruby.tar.gz
 cd ruby-${RUBY_VER}.${RUBY_VER_MINOR}
