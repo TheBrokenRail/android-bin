84adc32d10efb2c45e0fa48261467af84a8780e3
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-12-18 07:23:22 -0500

    Update build-ruby.sh

--- build-ruby.sh
+++ build-ruby.sh
@@ -6,8 +6,9 @@ RUBY_VER=2.5
 RUBY_VER_MINOR=3
 
 git clone https://github.com/rbenv/ruby-build.git
-PREFIX=/usr/local ruby-build/install.sh
-ruby-build ${RUBY_VER}.${RUBY_VER_MINOR} $(pwd)/ruby
+mkdir ruby-build/prefix
+PREFIX=ruby-build/prefix ruby-build/install.sh
+ruby-build/bin/ruby-build ${RUBY_VER}.${RUBY_VER_MINOR} $(pwd)/ruby
 
 OUT_DIR=${DEPLOY_DIR}/ruby
 mkdir ${OUT_DIR}
